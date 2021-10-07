1284ce57c033645b075a438771bb38d12dde9f00
Shailendra Karambe <karambe.shailendra@gmail.com>
2021-10-07 10:37:28 +0530

    Added test case for testing scenario B

--- src/test/java/com/promotion/PromotionEngineTest.java
+++ src/test/java/com/promotion/PromotionEngineTest.java
@@ -27,4 +27,22 @@ public class PromotionEngineTest {
         Assertions.assertEquals(100, cart.total());
         Assertions.assertEquals(0, cart.discount());
     }
+	
+	@Test
+    void testScenarioB() {
+        Item a = new Item(Product.A, 50, 5);
+        Item b = new Item(Product.B, 30, 5);
+        Item c = new Item(Product.C, 20, 1);
+        Cart cart = new Cart();
+        cart.add(a);
+        cart.add(b);
+        cart.add(c);
+        PromotionEngine promotionEngine = new PromotionEngine.Builder().addPromotion(new ProductAPromotion(true))
+                .addPromotion(new ProductBPromotion(true)).addPromotion(new ProductCAndDPromotion(true))
+                .addPromotionAggregator(new AllPromotionsAppliedAggregator()).build();
+        promotionEngine.apply(cart);
+        Assertions.assertEquals(370, cart.finalPrice());
+        Assertions.assertEquals(420, cart.total());
+        Assertions.assertEquals(50f, cart.discount());
+    }
 }
